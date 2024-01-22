a88d172ae7502835378996de10a982ccc068dfc5
acedryden <dryden.amy@gmail.com>
2024-01-22 17:38:43 -0500

    final updates

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,49 +1,101 @@
 # Import the dependencies.
 from flask import Flask, jsonify
-
-
+import datetime as dt
+import numpy as np 
+import pandas as pd 
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
 #################################################
 # Database Setup
 #################################################
-#app = Flask(__name__)
-
-
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 # reflect an existing database into a new model
+Base = automap_base()
 
 # reflect the tables
-
+Base.prepare(autoload_with=engine)
 
 # Save references to each table
-
+Meas = Base.classes.measurement
+Station = Base.classes.station
 
 # Create our session (link) from Python to the DB
-
+session = Session(engine)
 
 #################################################
 # Flask Setup
 #################################################
-
-
-
-
+app = Flask(__name__)
 #################################################
 # Flask Routes
 #################################################
-#@app.route("/")
-#start the home page, list all the available routes  
+@app.route("/")
+def main():
+    
+    return """
+    Welcome to my Hawaii Weather App! Please navigate to the correct page:<br>
+    /api/v1.0/precipitation - See Query Results of the last 12 months of Precipitation Data<br>
+    /api/v1.0/stations - See a list of the Weather Stations<br>
+    /api/v1.0/tobs - Displays the dates and temperatures of the most active weather station<br>
+    /api/v1.0/<start> Find the min temp, avg temp & temp temp for a specific start date<br>
+    /api/v1.0/<start>/<end> Find the min temp, avg temp & temp temp for a specific start & enddate<br>
+    """
+if __name__ == '__main__':
+    app.run()
 
-#@app.route("/api/v1.0/precipitation")
-#Convert the query results from the prec. analysis (last 12 months of data) to a dict using date as the key and prcp as the value
-#return the JSON representation of your dict 
 
-#@app.route("/api/v1.0/stations")
-#Return a JSON list of the stations from the dataset
+@app.route("/api/v1.0/precipitation")
+def precipitation(): 
+    session = Session(engine)
+    stations = session.query(Meas.prcp, Meas.date).all()
+    session.close()
 
-#@app.route("/api/v1.0/tobs")
-#query the dates and temp of the most-active station for the previous year
-#return a json list of temperature observations
-#@app.route("/api/v1.0/<start>")
-#return a JSON list of the min temp, avg temp and max temp for a sepecific start or start-end range. 
-#for a specified start, calculate tmin, tavg & tmax for all the dates greater than or equal to the start date. 
-#for a specified start and end date, calculate tmin, tavg and tmax for the dates from the start date to the end date, inclusive
-#@app.route("/api/v1.0/<start>/<end>")
+    output_prcp = []
+    for record in stations:
+        output_prcp.append(record[0])  
+    return jsonify(output_prcp)
+
+@app.route("/api/v1.0/stations")
+def stations():
+    session = Session(engine)
+    stations = session.query(Station.station).all()
+    session.close()
+
+    output_stations = []
+    for record in stations:
+        output_stations.append(record[0])  
+    return jsonify(output_stations)
+
+@app.route("/api/v1.0/tobs")
+def tobs(): 
+    session = Session(engine)
+    tobs = session.query(Meas.station,  Meas.tobs, Meas.date).filter(Meas.station == 'USC00519281').all()
+    session.close()
+
+    output_tobs = []
+    for record in tobs: 
+        output_tobs.append(record[0])
+    return jsonify(output_tobs)
+
+@app.route("/api/v1.0/<start>")
+def startdate():
+    session = Session(engine)
+    startdate = session.query(Meas.station, Meas.tobs, Meas.date).all()
+    session.close()
+
+    output_min = []
+    for record in tobs.min(): 
+        output_min.append(record[0])
+    return jsonify(output_min)
+    
+    output_max = []
+    for record in tobs.max(): 
+        output_min.append(record[0])
+    return jsonify(output_min)
+
+    output_avg = []
+    for record in tobs.avg():
+        output_avg.append(record[0])
+    return jsonify(output_avg)
